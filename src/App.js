11abe9d621e714b9bed13bc8effb1781ca51c7dc
motorniko <albertolagos59@gmail.com>
2024-02-18 02:10:21 -0300

    actualizando-archivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import logo1 from './imagenes/osiris.png';
 
 function App() {
   return (
     <div className='App'>
-      <div className='logo-cont'>
+      <div className='logo1-cont'>
         <img
-          src=''
+          src={logo1}
           className='logo1'
           alt='logo1' />
+      </div>
+
+      <div className='cont-calc'>
+
       </div>
     </div>
   );
